ffe56d7122e8d7af7aabeb77a1bb625989ed03f7
Sergio Oliveira <sergio@tracy.com.br>
2013-07-15 17:40:45 -0300

    Adding solr config

--- src/colab/local_settings-dev.py
+++ src/colab/local_settings-dev.py
@@ -8,6 +8,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 SOLR_COLAB_URI = None
+SOLR_HOSTNAME = None
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ')(jksdfhsjkadfhjkh234ns!8fqu-1186h$vuj'
