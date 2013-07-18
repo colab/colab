44c0fb2b55d4348185a9bf9af6fcd1fb76023ce3
Sergio Oliveira <sergio@tracy.com.br>
2013-07-18 12:48:10 -0300

    Fixing config

--- src/colab/custom_settings.py
+++ src/colab/custom_settings.py
@@ -57,6 +57,17 @@ SOLR_BASE_QUERY = """
     ((Type:changeset OR Type:ticket OR Type:wiki OR Type:thread) AND Title:["" TO *])
 """
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
+
 try:
     from local_settings import *
 except ImportError:
--- src/super_archives/views.py
+++ src/super_archives/views.py
@@ -6,7 +6,7 @@ from django.http import Http404
 from django.template import RequestContext
 from django.core.paginator import Paginator
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render_to_response, get_list_or_404
+from django.shortcuts import render, get_list_or_404
 
 from .models import MailingList, Thread
 
@@ -16,39 +16,38 @@ def thread(request, mailinglist, thread_token):
     try:
         first_message = queries.get_first_message_in_thread(mailinglist, thread_token)
     except ObjectDoesNotExist:
-        raise Http404 
+        raise Http404
     order_by = request.GET.get('order')
     if order_by == 'voted':
         msgs_query = queries.get_messages_by_voted()
     else:
         msgs_query = queries.get_messages_by_date()
-    
+
     msgs_query = msgs_query.filter(thread__subject_token=thread_token)
     msgs_query = msgs_query.filter(thread__mailinglist__name=mailinglist)
     emails = msgs_query.exclude(id=first_message.id)
-    
+
     total_votes = first_message.votes_count()
     for email in emails:
         total_votes += email.votes_count()
 
     # Update relevance score
     query = Thread.objects.filter(mailinglist__name=mailinglist)
-    thread = query.get(subject_token=thread_token)   
+    thread = query.get(subject_token=thread_token)
     thread.update_score()
- 
+
     template_data = {
         'first_msg': first_message,
         'emails': [first_message] + list(emails),
         'pagehits': queries.get_page_hits(request.path_info),
         'total_votes': total_votes,
     }
-    
-    return render_to_response('message-thread.html', template_data, 
-                              RequestContext(request))
+
+    return render(request, 'message-thread.html', template_data)
 
 
 def list_messages(request):
-    
+
     selected_list = request.GET.get('list')
 
     order_by = request.GET.get('order')
@@ -56,20 +55,20 @@ def list_messages(request):
         threads = queries.get_hottest_threads()
     else:
         threads = queries.get_latest_threads()
-    
+
     mail_list = request.GET.get('list')
     if mail_list:
         threads = threads.filter(mailinglist__name=mail_list)
-    
+
     paginator = Paginator(threads, 16)
     try:
         page = int(request.GET.get('p', '1'))
     except ValueError:
         page = 1
     threads = paginator.page(page)
-    
+
     lists = MailingList.objects.all()
-    
+
     template_data = {
         'lists': lists,
         'n_results': paginator.count,
@@ -77,5 +76,4 @@ def list_messages(request):
         'selected_list': selected_list,
         'order_by': order_by,
     }
-    return render_to_response('message-list.html', template_data, 
-                              RequestContext(request))
+    return render(request, 'message-list.html', template_data)
