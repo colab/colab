2523847d801afb4f70191638f58c1ecbec976a0f
Matheus Figueiredo <matheus@tracy.com.br>
2013-07-25 15:51:41 -0300

    Updating conversejs to development version

--- src/colab/static/conversejs/locale/locales.js
+++ src/colab/static/conversejs/locale/locales.js
@@ -1,11 +1,21 @@
+/*
+ * This file specifies the language dependencies.
+ *
+ * Translations take up a lot of space and you are therefore advised to remove
+ * from here any languages that you don't need.
+ */
+
 (function (root, factory) {
     require.config({
         paths: {
             "jed": "Libraries/jed",
             "af": "locale/af/LC_MESSAGES/af",
             "en": "locale/en/LC_MESSAGES/en",
+            "es": "locale/es/LC_MESSAGES/es",
             "de": "locale/de/LC_MESSAGES/de",
-            "hu": "locale/hu/LC_MESSAGES/hu"
+            "hu": "locale/hu/LC_MESSAGES/hu",
+            "it": "locale/it/LC_MESSAGES/it",
+            "ptbr": "locale/pt_BR/LC_MESSAGES/pt-br"
         }
     });
 
@@ -13,13 +23,19 @@
         'jed',
         'af',
         'en',
+        'es',
         'de',
-        'hu'
-        ], function (jed, af, en, de, hu) {
+        'hu',
+        "it",
+        'ptbr'
+        ], function (jed, af, en, es, de, hu, it, ptbr) {
             root.locales = {};
             root.locales.af = af;
             root.locales.en = en;
+            root.locales.es = es;
             root.locales.de = de;
             root.locales.hu = hu;
+            root.locales.it = it;
+            root.locales.ptbr = ptbr;
         });
 })(this);
