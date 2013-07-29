6739438d50d76ebee461c78403ebdb33405c44fd
Sergio Oliveira <sergio@tracy.com.br>
2013-07-29 11:03:21 -0300

    Configuring raven (sentry client)

--- src/colab/custom_settings.py
+++ src/colab/custom_settings.py
@@ -18,6 +18,7 @@ LANGUAGE_CODE = 'pt-br'
 INSTALLED_APPS = INSTALLED_APPS + (
 
     # Not standard apps
+    'raven.contrib.django.raven_compat',
     'south',
     'cliauth',
 
@@ -37,18 +38,51 @@ INSTALLED_APPS = INSTALLED_APPS + (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s',
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler',
             'include_html': True,
+        },
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
         }
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
+            'handlers': ['mail_admins', 'sentry'],
             'level': 'ERROR',
             'propagate': True,
+        },
+       'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['sentry'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
         },
     }
 }
