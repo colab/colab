a692ae54f17eb641c17a050475e1ada2c24df6a8
Sergio Oliveira <sergio@tracy.com.br>
2013-08-02 16:19:30 -0300

    Giving colab sudo rights and adding supervisor task

--- puppet/modules/colab/manifests/init.pp
+++ puppet/modules/colab/manifests/init.pp
@@ -11,14 +11,35 @@ class colab {
   include supervisor
   include colab::requirements
 
+  group { 'colab':
+    ensure => present,
+  }
+
   user { 'colab':
     ensure     => present,
     managehome => true,
     shell      => '/bin/bash',
+    gid        => 'colab',
+    groups     => ['sudo'],
   }
 
   mailalias { 'colab':
     ensure    => present,
     recipient => 'root',
+  }
+
+  file { 'colab-sudoers':
+    ensure  => present,
+    path    => '/etc/sudoers.d/colab-sudoers',
+    source  => 'puppet:///modules/colab/colab-sudoers',
+    mode    => '0440',
+    owner   => root,
+    group   => root,
+  }
+
+  supervisor::app { 'colab':
+    command   => '/home/colab/.virtualenvs/colab/bin/gunicorn_django colab/src/colab',
+    directory => '/home/colab/',
+    user      => 'colab',
   }
 }
