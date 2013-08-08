08fa10f3a8aace45437be25f504fe91f861651eb
Sergio Oliveira <sergio@tracy.com.br>
2013-08-08 18:00:52 -0300

    Importing socks from deprecated

--- src/colab/local_settings-dev.py
+++ src/colab/local_settings-dev.py
@@ -15,7 +15,7 @@ SOLR_HOSTNAME = None
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ')(jksdfhsjkadfhjkh234ns!8fqu-1186h$vuj'
 
-#import socks
+#from .deprecated import socks
 #SOCKS_TYPE = socks.PROXY_TYPE_SOCKS5
 #SOCKS_SERVER = '127.0.0.1'
 #SOCKS_PORT = 9050
