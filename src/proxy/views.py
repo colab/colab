016dd3f60452b079e92db0a8b82deb6ba7d5dcd4
Sergio Oliveira <sergio@tracy.com.br>
2013-10-01 12:23:57 -0300

    Fixing rules loader

--- src/proxy/views.py
+++ src/proxy/views.py
@@ -6,14 +6,19 @@ from django.conf import settings
 from revproxy.views import ProxyView
 
 
+CWD = os.path.abspath(os.path.dirname(__file__))
+DIAZO_RULES_DIR = os.path.join(CWD, 'diazo')
+
+
 class TracProxyView(ProxyView):
     base_url = settings.COLAB_TRAC_URL
     add_remote_user = settings.REVPROXY_ADD_REMOTE_USER
     diazo_theme_template = 'base.html'
-    diazo_rules = os.path.join(settings.BASE_DIR, 'proxy', 'trac_rules.xml')
+    diazo_rules = os.path.join(DIAZO_RULES_DIR, 'trac.xml')
 
 
 class JenkinsProxyView(ProxyView):
     base_url = settings.COLAB_CI_URL
     add_remote_user = settings.REVPROXY_ADD_REMOTE_USER
     diazo_theme_template = 'base.html'
+    diazo_rules = os.path.join(DIAZO_RULES_DIR, 'jenkins.xml')
