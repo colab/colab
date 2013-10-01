d9d6f997e3225bf5ac35f15542d46dedbed2abaa
Sergio Oliveira <sergio@tracy.com.br>
2013-10-01 11:59:37 -0300

    Fixing proxyview attr names

--- src/proxy/views.py
+++ src/proxy/views.py
@@ -9,11 +9,11 @@ from revproxy.views import ProxyView
 class TracProxyView(ProxyView):
     base_url = settings.COLAB_TRAC_URL
     add_remote_user = settings.REVPROXY_ADD_REMOTE_USER
-    diazo_template_theme = 'base.html'
+    diazo_theme_template = 'base.html'
     diazo_rules = os.path.join(settings.BASE_DIR, 'proxy', 'trac_rules.xml')
 
 
 class JenkinsProxyView(ProxyView):
     base_url = settings.COLAB_CI_URL
     add_remote_user = settings.REVPROXY_ADD_REMOTE_USER
-    diazo_template_theme = 'base.html'
+    diazo_theme_template = 'base.html'
