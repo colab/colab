9248e117b207c42d1ea34a3393a2671f92b27567
Sergio Oliveira <sergio@tracy.com.br>
2013-10-02 11:13:32 -0300

    Replacing trac navbar by dropdown in mainmenu

--- src/proxy/urls.py
+++ src/proxy/urls.py
@@ -6,8 +6,8 @@ from .views import TracProxyView, JenkinsProxyView
 
 urlpatterns = patterns('',
     # Trac URLs
-    url(r'^(?P<path>(?:admin|wiki|changeset|newticket|ticket|chrome|timeline|roadmap|browser|report|tags|query|about|prefs|log|attachment|raw-attachment).*)$',
-	TracProxyView.as_view()),
+    url(r'^(?P<path>(?:admin|wiki|changeset|newticket|ticket|chrome|timeline|roadmap|browser|report|tags|query|about|prefs|log|attachment|raw-attachment|diff).*)$',
+        TracProxyView.as_view()),
 
     # Jenkins URLs
     url(r'^ci/(?P<path>.*)$', JenkinsProxyView.as_view()),
