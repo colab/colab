3904d09c49d2335bc7f7e21fdd08b70c3893f4ca
Sergio Oliveira <sergio@tracy.com.br>
2013-10-17 19:11:39 -0300

    moving url template tag to superarchives file

--- src/super_archives/templatetags/superarchives.py
+++ src/super_archives/templatetags/superarchives.py
@@ -1,6 +1,8 @@
 
 from django import template
 
+from super_archives.utils import url
+
 
 register = template.Library()
 TEMPLATE_PATH = 'superarchives/tags/'
@@ -12,3 +14,21 @@ def display_message(email):
         email.update_blocks()
 
     return { 'blocks': email.blocks.all }
+
+
+@register.simple_tag(takes_context=True)
+def append_to_get(context, **kwargs):
+    return url.append_to_get(
+        context['request'].META['PATH_INFO'],
+        context['request'].META['QUERY_STRING'],
+        **kwargs
+    )
+
+
+@register.simple_tag(takes_context=True)
+def pop_from_get(context, **kwargs):
+    return url.pop_from_get(
+        context['request'].META['PATH_INFO'],
+        context['request'].META['QUERY_STRING'],
+        **kwargs
+    )
