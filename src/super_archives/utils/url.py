cefe623f0b25e45e3de3a8f0bc433cbd918d3399
Luan <luanpab@gmail.com>
2013-10-18 10:13:18 -0300

    Refactoring append_to_get to use urllib and urlparse

--- src/super_archives/utils/url.py
+++ src/super_archives/utils/url.py
@@ -1,63 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import urllib
+import urlparse
+
+
 def append_to_get(path, query=None, **kwargs):
-# Getting the path with the query
-    current_url = u'{}?{}'.format(
-        path,
-        query,
-    )
-
-    if kwargs and query:
-        current_url += '&'
-
+    query_dict = dict(urlparse.parse_qsl(query))
     for key, value in kwargs.items():
-        # get the key, value to check if the pair exists in the query
-        new = u'{}={}'.format(key, value)
-
-        if new in current_url:
-            continue
-
-        if u'&{}='.format(key) not in current_url:
-            current_url += u'{}={}&'.format(key, value)
-            continue
-
-        parse_url = current_url.split(key)
-
-        if len(parse_url) > 2:
-            continue
-
-        if unicode(value) in parse_url[1][1:]:
-            continue
-
-        check_kwargs_values = [
-            False for value in kwargs.values()
-            if unicode(value) not in parse_url[1]
-        ]
-
-        if not all(check_kwargs_values):
-            list_remaining = parse_url[1][1:].split('&')
-            real_remaining = u''
-
-            if len(list_remaining) >= 2:
-                real_remaining = u'&'.join(list_remaining[1:])
-
-            current_url = u'{url}{key}={value}&{remaining}'.format(
-                url=parse_url[0],
-                key=key,
-                value=value,
-                remaining=real_remaining,
-            )
-            continue
-
-        current_url = u'{url}{key}={value}+{remaining_get}'.format(
-            url=parse_url[0],
-            key=key,
-            value=value,
-            remaining_get=parse_url[1][1:],
-        )
-    if current_url[-1] == '&':
-        return current_url[:-1]
-    return current_url
+        query_dict[key] = value
+    return u'{}?{}'.format(path, urllib.urlencode(query_dict))
 
 
 def pop_from_get(path, query=None, **kwargs):
