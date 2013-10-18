006fe4a18d826ded14e4896ff251c8bece892faf
Luan <luanpab@gmail.com>
2013-10-18 15:16:59 -0300

    Fixing pop_from_get

--- src/super_archives/utils/url.py
+++ src/super_archives/utils/url.py
@@ -12,23 +12,13 @@ def append_to_get(path, query=None, **kwargs):
 
 
 def pop_from_get(path, query=None, **kwargs):
-    # Getting the path with the query
-    print query
-
-    current_url = u'{}?{}'.format(
-        path,
-        query,
-    )
+    query_dict = dict(urlparse.parse_qsl(query))
     for key, value in kwargs.items():
-        popitem = u'{}={}'.format(key, value)
-        if query == popitem:
-            return path
-
-        if key not in current_url:
-            return current_url
-
-        first_path, end_path = current_url.split(key)
-        end_path_without_element = end_path.split(value, 1)
-        path_list = first_path + end_path_without_element
-        print path_list
-        return u''.join(path_list)
+        if not query_dict.has_key(key):
+            continue
+        if query_dict[key] == value:
+            del query_dict[key]
+            continue
+    if not query_dict:
+        return u'{}?q='.format(path)
+    return u'{}?{}'.format(path, urllib.urlencode(query_dict))
