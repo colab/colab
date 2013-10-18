6f0134bf4d7cc0140f26449320cd7bb3367c35c8
Sergio Oliveira <sergio@tracy.com.br>
2013-10-18 12:33:23 -0300

    Removing search api

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,13 +1,9 @@
+
 from django.conf.urls import patterns, include, url
 
-from piston.resource import Resource
-
-from .handlers import SearchHandler
 from .views import VoteView
 
-search_handler = Resource(SearchHandler)
 
 urlpatterns = patterns('',
     url(r'message/(?P<msg_id>\d+)/vote$', VoteView.as_view()),
-    url(r'search/$', search_handler),
 )
