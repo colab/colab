379fe756028528554510b3d0a760463daed8eb97
Luan <luanpab@gmail.com>
2013-10-21 14:54:55 -0200

    Keeping value on filter inputs

--- src/search/views.py
+++ src/search/views.py
@@ -12,33 +12,94 @@ class ColabSearchView(SearchView):
             'wiki': {
                 'icon': 'file',
                 'name': _(u'Wiki'),
-                'fields': {'author': _(u'Author')},
+                'fields': (
+                    ('author', _(u'Author'), self.request.GET.get('author')),
+                ),
             },
             'thread': {
                 'icon': 'thread',
                 'name': _(u'Discussion'),
-                'fields': {'author': _(u'Author'), 'list': _(u'Mailinglist')},
+                'fields': (
+                    ('author', _(u'Author'), self.request.GET.get('author')),
+                    (
+                        'list',
+                        _(u'Mailinglist'),
+                        self.request.GET.getlist('list')
+                    ),
+                ),
             },
             'ticket': {
                 'icon': 'ticket',
                 'name': _(u'Ticket'),
-                'fields': {
-                    'milestone': _(u'Milestone'), 'priority': _(u'Priority'),
-                    'component': _(u'Component'), 'severity': _(u'Severity'),
-                    'reporter': _(u'Reporter'), 'author': _(u'Author'),
-                    'tag': _(u'Status'), 'keywords': _(u'Keywords'),
-                    'collaborators': _(u'Collaborators'),
-                },
+                'fields': (
+                    (
+                        'milestone',
+                        _(u'Milestone'),
+                        self.request.GET.get('milestone')
+                    ),
+                    (
+                        'priority',
+                        _(u'Priority'),
+                        self.request.GET.get('priority')
+                    ),
+                    (
+                        'component',
+                        _(u'Component'),
+                        self.request.GET.get('component')
+                    ),
+                    (
+                        'severity',
+                        _(u'Severity'),
+                        self.request.GET.get('severity')
+                    ),
+                    (
+                        'reporter',
+                        _(u'Reporter'),
+                        self.request.GET.get('reporter')
+                    ),
+                    ('author', _(u'Author'), self.request.GET.get('author')),
+                    ('tag', _(u'Status'), self.request.GET.get('tag')),
+                    (
+                        'keywords',
+                        _(u'Keywords'),
+                        self.request.GET.get('keywords'),
+                    ),
+                    (
+                        'collaborators',
+                        _(u'Collaborators'),
+                        self.request.GET.get('collaborators')
+                    ),
+                ),
             },
             'changeset': {
                 'icon': 'changeset',
                 'name': _(u'Changeset'),
-                'fields': {'author': _(u'Author'), 'repository_name': _(u'Repository')},
+                'fields': (
+                    ('author', _(u'Author'), self.request.GET.get('author')),
+                    (
+                        'repository_name',
+                        _(u'Repository'),
+                        self.request.GET.get('repository_name'),
+                    ),
+                )
             },
             'user': {
                 'icon': 'user',
                 'name': _(u'User'),
-                'fields': {'username': _(u'Username'), 'name': _(u'Name'), 'institution': _(u'Institution'), 'role': _(u'Role')},
+                'fields': (
+                    (
+                        'username',
+                        _(u'Username'),
+                        self.request.GET.get('username'),
+                    ),
+                    ('name', _(u'Name'), self.request.GET.get('name')),
+                    (
+                        'institution',
+                        _(u'Institution'),
+                        self.request.GET.get('institution'),
+                    ),
+                    ('role', _(u'Role'), self.request.GET.get('role'))
+                ),
             },
         }
 
