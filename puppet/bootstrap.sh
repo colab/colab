915e79f8b32bcaeecbfaf885eb94a80fc71287f1
Sergio Oliveira <sergio@tracy.com.br>
2013-10-24 23:48:20 -0200

    Updating bootstrap script

--- puppet/bootstrap.sh
+++ puppet/bootstrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PUPPET_VERSION=`dpkg -l | grep puppet-common | awk '{ print $3 }'`
+PUPPET_VERSION=`dpkg -l | grep puppet-common | awk '{ print $3 }' | cut -d- -f1`
+dpkg --compare-versions "$PUPPET_VERSION" "<" "3.3" ; OUTDATED=$?
 
-if [ "$PUPPET_VERSION" != '3.2.3-1puppetlabs1' ] ; then 
+if [ $OUTDATED -eq 0 ] ; then
     wget -O /tmp/puppet_apt.deb http://apt.puppetlabs.com/puppetlabs-release-precise.deb &> /dev/null
     dpkg -i /tmp/puppet_apt.deb
     DEBIAN_FRONTEND=noninteractive apt-get update -y
