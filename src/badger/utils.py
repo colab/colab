b615983c3d4302cb6f7711ad59b7ec825dee130f
Luan <luanpab@gmail.com>
2013-11-08 13:52:31 -0200

    Changing filters to use haystack

--- src/badger/utils.py
+++ src/badger/utils.py
@@ -2,15 +2,28 @@
 
 from django.db.models import Count
 
+from haystack.query import SearchQuerySet
+
 from proxy.models import Revision, Ticket, Wiki
 from super_archives.models import Message
 
 
 def get_counters_to_badge(user):
-    count_revisions = Revision.objects.filter(author=user.username).count()
-    count_tickets = Ticket.objects.filter(author=user.username).count()
-    count_wikis = Wiki.objects.filter(author=user.username).count()
-
+    # count_revisions = Revision.objects.filter(author=user.username).count()
+    # count_tickets = Ticket.objects.filter(author=user.username).count()
+    # count_wikis = Wiki.objects.filter(author=user.username).count()
+    count_revisions = SearchQuerySet().filter(
+        type='changeset',
+        author=user.username
+    ).count()
+    count_tickets = SearchQuerySet().filter(
+        type='ticket',
+        author=user.username
+    ).count()
+    count_wikis = SearchQuerySet().filter(
+        type='wiki',
+        author=user.username
+    ).count()
     return dict(
         messages=user.emails.aggregate(Count('message'))['message__count'],
         revisions=count_revisions,
@@ -18,10 +31,3 @@ def get_counters_to_badge(user):
         wikis=count_wikis,
         contributions=count_revisions + count_tickets + count_wikis,
     )
-
-# using haystack
-# sqs = SearchQuerySet()
-# sqs.filter(type='changeset', author=user.get_full_name()).count()
-# sqs.filter(type='wiki', author=user.get_full_name()).count()
-# sqs.filter(type='ticket', author=user.get_full_name()).count()
-# Should it use the author_and_username attr too?
