7b200a817f3fba4dca3f67b3cdc9c8790ff3e011
Luan <luanpab@gmail.com>
2013-12-06 14:54:02 -0200

    Removing time restriction on home chart - closes #84

--- src/home/views.py
+++ src/home/views.py
@@ -2,7 +2,6 @@
 from collections import OrderedDict
 
 from django.shortcuts import render
-from django.utils import timezone
 
 from search.utils import trans
 from haystack.query import SearchQuerySet
@@ -18,11 +17,9 @@ def index(request):
     hottest_threads = Thread.highest_score.from_haystack()[:6]
 
     count_types = OrderedDict()
-    six_months = timezone.now() - timezone.timedelta(days=180)
     for type in ['thread', 'ticket', 'wiki', 'changeset', 'attachment']:
         count_types[trans(type)] = SearchQuerySet().filter(
             type=type,
-            modified__gte=six_months,
         ).count()
 
     context = {
