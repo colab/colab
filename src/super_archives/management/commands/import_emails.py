12b5905452741b931abc6a5e7efcdee8fbbafd2d
Luan <luanpab@gmail.com>
2013-12-12 09:42:19 -0200

    Minor fix on import_emails

--- src/super_archives/management/commands/import_emails.py
+++ src/super_archives/management/commands/import_emails.py
@@ -11,7 +11,7 @@ from optparse import make_option
 
 from django.db import transaction
 from django.template.defaultfilters import slugify
-from super_archives.utils.message import colab_send_email
+from super_archives.utils.email import colab_send_email
 from django.core.management.base import BaseCommand, CommandError
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext as _
