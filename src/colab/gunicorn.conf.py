ab6ac0f35a5490ace6d6d1c6c6990f8d959946bc
Lucas Kanashiro <kanashiro.duarte@gmail.com>
2014-07-31 16:38:56 -0300

    Remove dep and changed gevent to eventlet

--- src/colab/gunicorn.conf.py
+++ src/colab/gunicorn.conf.py
@@ -1,7 +1,6 @@
 
 import multiprocessing
 
-bind = "127.0.0.1:8000"
-workers = multiprocessing.cpu_count() + 1
+workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 300
-worker_class = 'gevent'
+worker_class = 'eventlet'
