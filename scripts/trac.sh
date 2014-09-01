ac3ed4bef9e401a67cf47714edd2c1a76412b33b
Alexandre Barbosa <alexandreab@live.com>
2014-09-01 15:12:06 -0300

    Updating Trac script to create database
    Updating conf file for supervisor
    
    Updating Trac script

--- scripts/trac.sh
+++ scripts/trac.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [[ ! "$1" ]]
+        then
+        echo " Parametro nao encontrado."
+        echo " ./tra.sh <DATABASE_HOST>"
+        exit -1
+fi
+
+DATABASE_HOST=$1
+
+sudo rm -rf /opt/trac
+
+if [[ $DATABASE_HOST == "127.0.0.1" ]]; then
+	sudo -u postgres psql -c "DROP DATABASE trac_colab;"
+	sudo -u postgres psql -c "CREATE DATABASE trac_colab WITH OWNER colab ENCODING 'UTF8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8' TEMPLATE=template0;"
+fi
+
 sudo yum install -y wget
 sudo wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6 https://www.fedoraproject.org/static/0608B895.txt
 sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
@@ -15,7 +31,7 @@ sudo easy_install psycopg2
 sudo yum install -y trac
 
 sudo mkdir -p /opt/trac
-sudo trac-admin /opt/trac initenv colab postgres://colab:colab@/trac_colab?host=$1
+sudo trac-admin /opt/trac initenv colab postgres://colab:colab@/trac_colab?host=$DATABASE_HOST
 
 sudo yum install subversion -y
 
@@ -30,7 +46,7 @@ directory=/opt/trac
 user=root
 autostart=true
 autorestart=false
-redirect_stderr=True\" >> /etc/supervisord.conf"
+redirect_stderr=True\" > /etc/supervisor/conf.d/trac.conf"
 
 sudo service supervisord start
 
