e6ab39a029c9084295a7f9f8b9e0b1a9673add4a
Sergio Oliveira <sergio@tracy.com.br>
2014-09-23 11:04:40 -0300

    Specific settings for QA (or prod?) environment

--- src/colab/local_settings-digital.py
+++ src/colab/local_settings-digital.py
@@ -1,39 +1,50 @@
+
+import os
+import json
+
 from custom_settings import *
 
-DEBUG = True
+SECRETS_FILE = '/home/colab/colab/secrets.json'
+
+if os.path.exists(SECRETS_FILE):
+    secrets = json.load(file(SECRETS_FILE))
+
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
+    ('Paulo Meirelles', 'paulo@softwarelivre.org'),
 )
 
 MANAGERS = ADMINS
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_HOST = ''
-#EMAIL_PORT = 587
+COLAB_FROM_ADDRESS = '"Portal do Software Publico" <noreply@beta.softwarepublico.gov.br>'
+SERVER_EMAIL = COLAB_FROM_ADDRESS
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
 EMAIL_SUBJECT_PREFIX = ''
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ')(jksdfhsjkadfhjkh234ns!8fqu-1186h$vuj'
+SECRET_KEY = secrets.get('SECRET_KEY')
 
-SITE_URL = os.environ.get('COLAB_HOST')
+SITE_URL = 'http://beta.softwarepublico.gov.br'
 BROWSERID_AUDIENCES = [SITE_URL, SITE_URL.replace('https', 'http')]
 
-ALLOWED_HOSTS = [os.environ.get('COLAB_HOST')]
+ALLOWED_HOSTS = ['beta.softwarepublico.gov.br']
 
 INTERNAL_IPS = ('127.0.0.1', )
 
-CONVERSEJS_BOSH_SERVICE_URL = 'http://localhost:5280/http-bind'
-
-DATABASES['default']['PASSWORD'] = os.environ.get('COLAB_DB_PWD')
+DATABASES['default']['PASSWORD'] = secrets.get('COLAB_DB_PWD')
 DATABASES['default']['HOST'] = 'localhost'
 
-TRAC_ENABLED = False
+TRAC_ENABLED = True
 
 if TRAC_ENABLED:
     from trac_settings import *
     DATABASES['trac'] = TRAC_DATABASE
-    DATABASES['trac']['PASSWORD'] = os.environ.get('TRAC_DB_PWD')
+    DATABASES['trac']['PASSWORD'] = secrets.get('TRAC_DB_PWD')
     DATABASES['trac']['HOST'] = 'localhost'
 
 HAYSTACK_CONNECTIONS['default']['URL'] = 'http://localhost:8983/solr/'
@@ -48,3 +59,7 @@ CONVERSEJS_ENABLED = False
 DIAZO_THEME = SITE_URL
 
 ROBOTS_NOINDEX = True
+
+RAVEN_CONFIG = {
+    'dsn': secrets.get('RAVEN_DSN', '') + '?timeout=30',
+}
