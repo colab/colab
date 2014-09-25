3ac70c1adca7ba05790ec33cf1125c91463738ce
Sergio Oliveira <sergio@tracy.com.br>
2014-09-25 18:21:53 -0300

    Fixing migration dependency

--- src/super_archives/migrations/0001_initial.py
+++ src/super_archives/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('taggit', '0002_auto_20140925_1819'),
+        ('taggit', '0001_initial'),
     ]
 
     operations = [
