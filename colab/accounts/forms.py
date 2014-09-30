2d26e024a0de34b6a89e4705058353c40bde8d74
Macartur Sousa <macartur.sc@gmail.com>
2014-09-30 08:44:43 -0400

    Adding confirm password
    Adding confirm password

--- colab/accounts/forms.py
+++ colab/accounts/forms.py
@@ -50,6 +50,10 @@ class UserForm(forms.ModelForm):
 
 class UserCreationForm(UserForm):
 
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username')
+
     def clean_username(self):
         username = self.cleaned_data['username']
         username = username.strip()
@@ -57,9 +61,32 @@ class UserCreationForm(UserForm):
             raise forms.ValidationError(_('This field cannot be blank.'))
         return username
 
+
+class UserCreationFormNoBrowserId(UserCreationForm):
+
+    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Confirm Password "), widget=forms.PasswordInput)
+
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username')
+        fields = ('first_name', 'last_name','email', 'username')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+                raise forms.ValidationError(_("The two password fields didn't match."))
+        return password2 
+
+    def save(self, commit=True):
+        """
+	Saves the new password.
+	"""
+	self.instance.set_password(self.cleaned_data["password1"])
+	if commit:
+	    self.instance.save()
+	return self.instance
 
 
 class UserUpdateForm(UserForm):
