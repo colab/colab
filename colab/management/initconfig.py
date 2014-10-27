898146cfa0a37d04e8f123eb9fba182be099ee2b
Sergio Oliveira <sergio@tracy.com.br>
2014-10-27 16:28:09 -0200

    Commented out example.com domains from settings

--- colab/management/initconfig.py
+++ colab/management/initconfig.py
@@ -31,15 +31,15 @@ SECRET_KEY: '{secret_key}'
 SITE_URL: 'http://localhost:8000/'
 BROWSERID_AUDIENCES:
   - http://localhost:8000
-  - http://example.com
-  - https://example.org
-  - http://example.net
+#  - http://example.com
+#  - https://example.org
+#  - http://example.net
 
 ALLOWED_HOSTS:
   - localhost
-  - example.com
-  - example.org
-  - example.net
+#  - example.com
+#  - example.org
+#  - example.net
 
 ### Uncomment to enable Converse.js
 # CONVERSEJS_ENABLED: True
