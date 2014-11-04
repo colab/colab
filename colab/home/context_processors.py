5f8058ed71591ef25d44e7804bea5918f7adb33e
Sergio Oliveira <sergio@tracy.com.br>
2014-11-04 16:02:40 -0200

    Fixed spacing (PEP8)

--- colab/home/context_processors.py
+++ colab/home/context_processors.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
+
 def robots(request):
     return {'ROBOTS_NOINDEX': getattr(settings, 'ROBOTS_NOINDEX', False)}
+
 
 def ribbon(request):
     enabled = getattr(settings, 'RIBBON_ENABLED', True)
