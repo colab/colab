749678059710b5f3c9c456a275fa057d0846a19f
Sergio Oliveira <sergio@tracy.com.br>
2014-11-05 15:44:58 -0200

    Installed pip and virtualenvwrapper using apt

--- vagrant/ubuntu.sh
+++ vagrant/ubuntu.sh
@@ -5,19 +5,8 @@ set -ex
 ### Install dependencies
 apt-get update
 
-apt-get install curl git unzip mercurial build-essential libev-dev gettext libxml2-dev libxslt1-dev libssl-dev libffi-dev libjpeg-dev zlib1g-dev libfreetype6-dev libpq-dev python-dev postgresql -y
+apt-get install curl git unzip mercurial build-essential libev-dev gettext libxml2-dev libxslt1-dev libssl-dev libffi-dev libjpeg-dev zlib1g-dev libfreetype6-dev libpq-dev python-dev postgresql virtualenvwrapper python-pip -y
 
-
-### Install Virtualenvwrapper
-which pip2.7 > /dev/null ||
-    curl -s -L https://raw.githubusercontent.com/pypa/pip/1.5.6/contrib/get-pip.py |
-        python2.7
-
-if [ ! -L /etc/bash_completion.d/virtualenvwrapper.sh ]
-then
-    pip install virtualenvwrapper
-    ln -s /usr/local/bin/virtualenvwrapper.sh /etc/bash_completion.d/virtualenvwrapper.sh
-fi
 
 ### Create conf directory
 mkdir -p /etc/colab
