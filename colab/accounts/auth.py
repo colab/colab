3e89bbc3f87eae2545c1add5710ad7b089d2278a
Carlos Oliveira <carlospecter@gmail.com>
2014-12-16 09:26:28 -0200

    Reimplemented way to create user through browserid
    Added more fields to create user with custom model
    
    Signed-off-by: Carlos Oliveira <carlospecter@gmail.com>
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- colab/accounts/auth.py
+++ colab/accounts/auth.py
@@ -1,6 +1,28 @@
+import re
 
 from django_browserid.auth import BrowserIDBackend
+
 
 class ColabBrowserIDBackend(BrowserIDBackend):
     def filter_users_by_email(self, email):
         return self.User.objects.filter(emails__address=email)
+
+    def create_user(self, email):
+        username = "colab_" + re.split('@', email)[0]
+        password = None
+        extra_fields = {
+            'first_name': "Colab",
+            'last_name': "Colab",
+            }
+
+        user = self.User.objects.filter(emails__address=email)
+
+        if len(user) is not 0:
+            return user
+
+        return self.User.objects.create_user(
+            username,
+            email,
+            password,
+            **extra_fields
+            )
