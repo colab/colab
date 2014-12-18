142f60ef6fd41473748b43a9ad994a9a4f0cf6cd
Sergio Oliveira <sergio@tracy.com.br>
2014-12-18 15:43:36 -0200

    Removed create_user method

--- colab/accounts/auth.py
+++ colab/accounts/auth.py
@@ -1,25 +1,7 @@
-from django_browserid.auth import BrowserIDBackend, default_username_algo
+
+from django_browserid.auth import BrowserIDBackend
 
 
 class ColabBrowserIDBackend(BrowserIDBackend):
     def filter_users_by_email(self, email):
         return self.User.objects.filter(emails__address=email)
-
-    def create_user(self, email):
-        username = default_username_algo(email)
-        password = None
-        extra_fields = {
-            'needs_update': True
-            }
-
-        user = self.User.objects.filter(emails__address=email)
-
-        if user:
-            return user
-
-        return self.User.objects.create_user(
-            username,
-            email,
-            password,
-            **extra_fields
-            )
