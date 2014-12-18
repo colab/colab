d200e8cf60ef0f889e0c97fb0e1cb0babedb2e4f
Sergio Oliveira <sergio@tracy.com.br>
2014-12-18 16:24:38 -0200

    Allowing users to logout

--- colab/accounts/middleware.py
+++ colab/accounts/middleware.py
@@ -1,19 +1,21 @@
-from django.core.urlresolvers import resolve
+
 from django.shortcuts import redirect
+
+VIEW_NAMES_ALLOWED = ('signup', 'Logout')
 
 
 class UserRegisterMiddleware(object):
 
     def process_view(self, request, view_func, view_args, view_kwargs):
+
+        if request.is_ajax():
+            return
+
         if not request.user.is_authenticated():
             return
 
         if not request.user.needs_update:
             return
 
-        current_url = resolve(request.path_info).url_name
-
-        if current_url not in ['signup']:
+        if view_func.__name__ not in VIEW_NAMES_ALLOWED:
             return redirect('signup')
-
-        return None
