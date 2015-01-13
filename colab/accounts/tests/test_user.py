a7a92bb6623ad60d3fc294364c9f02a65faff473
msfernandes <matheus.souza.fernandes@gmail.com>
2015-01-13 17:27:47 -0200

    Testing update user information

--- colab/accounts/tests/test_user.py
+++ colab/accounts/tests/test_user.py
@@ -3,13 +3,17 @@ Test User class.
 Objective: Test parameters, and behavior.
 """
 from colab.accounts.models import User
-from django.test import TestCase
+from django.test import TestCase, Client
 
 
 class UserTest(TestCase):
 
     def setUp(self):
         self.user = self.create_user()
+        self.client = Client()
+
+    def tearDown(self):
+        pass
 
     def create_user(self):
         user = User()
@@ -19,9 +23,18 @@ class UserTest(TestCase):
         user.id = 1
         user.twitter = "usertestcolab"
         user.facebook = "usertestcolab"
+        user.first_name = "USERtestCoLaB"
+        user.last_name = "COLAB"
         user.save()
 
         return user
+
+    def authenticate_user(self):
+        self.user.needs_update = False
+        self.user.save()
+
+        self.client.login(username=self.user.username,
+                          password='123colab4')
 
     def test_check_password(self):
         self.assertTrue(self.user.check_password("123colab4"))
@@ -54,3 +67,42 @@ class UserTest(TestCase):
         user_db = User.objects.get(id=1)
         self.assertEqual(user_db.username, username_test.lower())
         self.user.delete()
+
+    def test_update_user_mandatory_information(self):
+        self.authenticate_user()
+
+        data = {'first_name': 'usercolabtest',
+                'last_name': 'colabtest'}
+        self.client.post('/account/usertestcolab/edit', data)
+
+        user = User.objects.get(id=1)
+
+        self.assertEqual('usercolabtest', user.first_name)
+        self.assertEqual('colabtest', user.last_name)
+        self.user.delete()
+
+    def test_update_user_mandatory_invalid_information(self):
+        self.authenticate_user()
+
+        data = {'first_name': 'a' * 31,
+                'last_name': 'a' * 31}
+        self.client.post('/account/usertestcolab/edit', data)
+
+        user = User.objects.get(id=1)
+
+        self.assertEqual('USERtestCoLaB', user.first_name)
+        self.assertEqual('COLAB', user.last_name)
+
+    def test_update_user_all_information(self):
+        self.authenticate_user()
+
+        data = {'first_name': 'Colab',
+                'last_name': 'Test',
+                'institution': 'fga',
+                'role': 'tester',
+                'twitter': 'twitter',
+                'facebook': 'facebook',
+                'google_talk': 'gtalk',
+                'github': 'github',
+                'webpage': 'webpage',
+                'bio': 'bio'}
