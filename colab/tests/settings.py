9cfb635b9d1a1aadb3e63acfad223e8e3f15810a
Sergio Oliveira <sergio@tracy.com.br>
2015-01-15 19:08:34 -0200

    Silent mailman error logs for tests

--- colab/tests/settings.py
+++ colab/tests/settings.py
@@ -1,5 +1,30 @@
 from ..settings import *
 
+LOGGING = {
+    'version': 1,
+
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+    },
+
+    'loggers': {
+        'colab.mailman': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+        'haystack': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+        'pysolr': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+    },
+}
 
 import os
 HAYSTACK_CONNECTIONS = {
