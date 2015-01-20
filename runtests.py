490a4b86c65243a7517c29ba167afe68dcb1c2cc
carol15022 <carol15022@hotmail.com>
2015-01-20 09:29:30 -0200

    Script of test with coverage. Signed-off-by: Matheus Fernandes <matheus.fernandes@gmail.com> Signed-off-by: Sergio Oliveira <sergio@tracy.com.br>

---
+++ runtests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'colab.tests.settings'
+os.environ['COVERAGE_PROCESS_START'] = '.coveragerc'
+os.environ['REUSE_DB'] = '0'
+
+import django
+import coverage
+
+from django.test.utils import get_runner
+from django.conf import settings
+
+
+def runtests():
+    if django.VERSION >= (1, 7, 0):
+        django.setup()
+    test_runner = get_runner(settings)
+    failures = test_runner(interactive=False, failfast=False).run_tests([])
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    coverage.process_startup()
+    runtests()
--- runtests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DJANGO_SETTINGS_MODULE="colab.tests.settings"
-
-django-admin test
