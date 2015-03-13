164f1f37edd3c2520f4773be7f86c1b7305c5aad
Sergio Oliveira <sergio@tracy.com.br>
2015-03-13 18:06:58 -0300

    Send package to packagecloud

--- ci/build_rpm.sh
+++ ci/build_rpm.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$TRAVIS_BRANCH" != "ci-package" ]; then
+  exit 0;
+fi
+
 python setup.py sdist
 
 sudo apt-get install rinse
@@ -17,3 +21,9 @@ sudo cp ci/softwarepublico.key /tmp/centos-7/etc/yum.repos.d/
 sudo chroot /tmp/centos-7/ yum install rpm-build -y
 sudo chroot /tmp/centos-7/ yum install python-virtualenv colab-deps -y
 sudo HOME=/root chroot /tmp/centos-7/ rpmbuild -ba /root/rpmbuild/SPECS/colab.spec
+
+# Send to packagecloud
+
+gem install package_cloud
+PACKAGE_PATH=`sudo find /tmp/centos-7/root/rpmbuild/RPMS/noarch/ -name "colab*.rpm"`
+package_cloud push seocam/colab-$TRAVIS_BRANCH/el/7 $PACKAGE_PATH
