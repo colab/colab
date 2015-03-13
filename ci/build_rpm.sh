203de6127b37d1457d5f899d614065ce0053f44f
Sergio Oliveira <sergio@tracy.com.br>
2015-03-13 17:40:50 -0300

    Removed set -e for build_rpm

--- ci/build_rpm.sh
+++ ci/build_rpm.sh
@@ -14,8 +14,6 @@ sudo cp ci/softwarepublico.key /tmp/centos-7/etc/yum.repos.d/
 
 # Commands on chroot
 
-set -e
-
 sudo chroot /tmp/centos-7/ yum install rpm-build -y
 sudo chroot /tmp/centos-7/ yum install python-virtualenv colab-deps -y
 sudo HOME=/root chroot /tmp/centos-7/ rpmbuild -ba /root/rpmbuild/SPECS/colab.spec
