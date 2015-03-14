78f03b20e41fac736b278aa096514ea7f24445f9
Sergio Oliveira <sergio@tracy.com.br>
2015-03-14 05:02:22 -0300

    define must come before .spec

--- ci/build_rpm.sh
+++ ci/build_rpm.sh
@@ -44,7 +44,7 @@ fi
 
 echo "Building package: $version-$release"
 
-sudo HOME=/root chroot /tmp/centos-7/ rpmbuild -ba /root/rpmbuild/SPECS/colab.spec --define="release ${release}"
+sudo HOME=/root chroot /tmp/centos-7/ rpmbuild --define "release $release" -ba /root/rpmbuild/SPECS/colab.spec --quiet
 sudo cp /tmp/centos-7/root/rpmbuild/RPMS/noarch/colab-$version-$release.noarch.rpm .
 
 
