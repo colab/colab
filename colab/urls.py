7961b4425b8f7ed88810b570370b8636670f5c8e
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-06-02 16:21:17 -0300

    Fixed urls for Mezuro
    Without this, Colab was unable to request assets or any other sub page from
    Mezuro.
    
    This is not the correct fix. The right place to set the url is on:
    colab/plugins/mezuro/urls.py. But this is not working yet.
    
    Notice that we've also identified a bug on the ReverseProxy, which is not
    repecting the upstream port always trying to request from port 80.
    
    Signed off by: Gustavo Jaruga <darksshades@hotmail.com>

--- colab/urls.py
+++ colab/urls.py
@@ -37,6 +37,7 @@ urlpatterns = patterns('',
 
     url(r'^trac/', include('colab.plugins.trac.urls')),
     url(r'^gitlab/', include('colab.plugins.gitlab.urls')),
+    url(r'^mezuro/', include('colab.plugins.mezuro.urls')),
     url(r'^social/', include('colab.plugins.noosfero.urls')),
     url(r'^ci/', include('colab.plugins.jenkins.urls')),
 
