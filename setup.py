8436db93f25d8fd69286e532b879adbf5641bfd0
Sergio Oliveira <sergio@tracy.com.br>
2015-06-08 14:58:24 -0300

    Allow higher versions from deps
    Signed-off-by: Sergio Oliveira <sergio@tracy.com.br>
    Signed-off-by: Carolina Ramalho <carol15022@hotmail.com>
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>

--- setup.py
+++ setup.py
@@ -3,48 +3,48 @@ from setuptools import setup, find_packages
 
 
 REQUIREMENTS = [
-    'Django==1.7.7',
+    'Django>=1.7.8',
     'pytz>=2011n',
-    'django-mobile==0.3.0',
-    'django-haystack==2.2',
-    'pysolr==2.1',
-    'django-hitcounter==0.1.1',
-    'django-tastypie==0.11.0',
+    'django-mobile>=0.3.0',
+    'django-haystack>=2.2',
+    'pysolr>=2.1',
+    'django-hitcounter>=0.1.1',
+    'django-tastypie>=0.12.1',
 
     # Deps for Single SignOn (SSO) - Replaced with django-browserid==0.9
-    'django-browserid==0.11',
-    'django-revproxy==0.9.0',
+    'django-browserid>=0.11',
+    'django-revproxy>=0.9.0',
 
     # Diazo
-    'diazo==1.0.5',
+    'diazo>=1.0.5',
 
     ### Move out of colab (as plugins):
 
     # Deps for badger
-    'Pillow==2.5.1',
-    'django-i18n-model==0.0.7',
+    'Pillow==2.8.1',
+    'django-i18n-model>=0.0.7',
 
     # Deps for super_archives
     'etiquetando==0.1',
-    'django-taggit==0.12.1',
-    'html2text==3.200.3',
+    'django-taggit>=0.12.1',
+    'html2text>=3.200.3',
     'chardet>=1.0.1',
 
     # Deps for gitlab plugin
-    'python-dateutil==1.5',
+    'python-dateutil>=1.5',
 
     # Converse.js (XMPP client)
-    'django-conversejs==0.3.4',
+    'django-conversejs>=0.3.4',
 
     # Mailman 2 REST API
-    'mailman-api==0.2.9',
+    'mailman-api>=0.2.9',
 ]
 
 TEST_REQUIREMENTS = [
-    'coverage==3.7.1',
-    'coveralls==0.5',
-    'flake8==2.3.0',
-    'mock==1.0.1',
+    'coverage>=3.7.1',
+    'coveralls>=0.5',
+    'flake8>=2.3.0',
+    'mock>=1.0.1',
 ]
 
 
