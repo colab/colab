f758f049d9b092fe84826263648bc32901e97442
Sergio Oliveira <sergio@tracy.com.br>
2015-08-04 19:54:02 -0300

    Running behave with setup.py test

--- tests/run.py
+++ tests/run.py
@@ -12,15 +12,18 @@ os.environ['COVERAGE_PROCESS_START'] = '.coveragerc'
 import django
 import coverage
 
-from django.test.utils import get_runner
 from django.conf import settings
+from django.core.management import execute_from_command_line
+from django.test.utils import get_runner
 
 
 def runtests():
     if django.VERSION >= (1, 7, 0):
         django.setup()
+
     test_runner = get_runner(settings)
     failures = test_runner(interactive=False, failfast=False).run_tests([])
+    execute_from_command_line(['colab-admin', 'behave'])
     sys.exit(failures)
 
 
@@ -32,4 +35,4 @@ def run_with_coverage():
 
 
 if __name__ == '__main__':
-    runtests_with_coverage()
+    run_with_coverage()
