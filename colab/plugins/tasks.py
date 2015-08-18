64781e7e895891b0c97fb41cf66e8c63121af958
Sergio Oliveira <sergio@tracy.com.br>
2015-08-18 01:13:13 -0300

    Allow full plugins on /etc/colab/plugins.d
    Signed-off-by: Sergio Oliveira <sergio@tracy.com.br>

--- colab/plugins/tasks.py
+++ colab/plugins/tasks.py
@@ -2,10 +2,10 @@
 from datetime import timedelta
 from celery.decorators import periodic_task
 
-from .utils import data
+from .data import TASKS
 
 
-@periodic_task(run_every=timedelta(minutes=1))
+@periodic_task(run_every=timedelta(seconds=5))
 def import_plugin_data():
-    for task in data.TASKS:
+    for task in TASKS:
         task.delay()
