4cb38792a658de22b93e942d1632b3d0ac6cfb3b
Gust <darksshades@hotmail.com>
2015-08-20 10:28:30 -0300

    Change periodic task to 60 seconds

--- colab/plugins/tasks.py
+++ colab/plugins/tasks.py
@@ -5,7 +5,7 @@ from celery.decorators import periodic_task
 from .data import TASKS
 
 
-@periodic_task(run_every=timedelta(seconds=5))
+@periodic_task(run_every=timedelta(seconds=60))
 def import_plugin_data():
     for task in TASKS:
         task.delay()
