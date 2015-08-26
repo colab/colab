b7e59f6bd142d1e0383e2d80f165e26d678d2d6b
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-08-26 15:57:17 -0300

    Finished gitlab plugin tests
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- colab/plugins/gitlab/tests/test_data_importer.py
+++ colab/plugins/gitlab/tests/test_data_importer.py
@@ -1,21 +1,17 @@
 from django.test import TestCase
 from django.test.utils import override_settings
 from ..data_importer import GitlabDataImporter
-from ..models import GitlabProject
+from ..models import GitlabProject, GitlabIssue
 from mock import patch
 import data
+from dateutil.parser import parse
 
 
 class GitlabDataImporterTest(TestCase):
 
     fixtures = ["gitlab_associations"]
 
-    colab_apps = data.colab_apps
-    projects_json = data.projects_json
-    groups_json = data.groups_json
-    merge_json = data.merge_json
-
-    @override_settings(COLAB_APPS=colab_apps)
+    @override_settings(COLAB_APPS=data.colab_apps)
     def setUp(self):
         self.api = GitlabDataImporter()
 
@@ -31,7 +27,7 @@ class GitlabDataImporterTest(TestCase):
 
     @patch.object(GitlabDataImporter, 'get_json_data')
     def test_fetch_projects(self, mock_json):
-        mock_json.side_effect = [self.projects_json, []]
+        mock_json.side_effect = [data.projects_json, []]
 
         projects = self.api.fetch_projects()
         self.assertEqual(len(projects), 1)
@@ -41,7 +37,7 @@ class GitlabDataImporterTest(TestCase):
 
     @patch.object(GitlabDataImporter, 'get_json_data')
     def test_fetch_groups(self, mock_json):
-        mock_json.side_effect = [self.groups_json, []]
+        mock_json.side_effect = [data.groups_json, []]
 
         groups = self.api.fetch_groups()
         self.assertEqual(len(groups), 2)
@@ -53,10 +49,49 @@ class GitlabDataImporterTest(TestCase):
 
     @patch.object(GitlabDataImporter, 'get_json_data')
     def test_fetch_merge(self, mock_json):
-        mock_json.side_effect = [self.merge_json, []]
+        mock_json.side_effect = [data.merge_json, []]
 
         merges = self.api.fetch_merge_request([GitlabProject()])
         self.assertEqual(len(merges), 1)
         self.assertEqual(merges[0].title, "Merge Title")
         self.assertEqual(merges[0].description, "description")
         self.assertEqual(merges[0].get_author().username, "user")
+
+    @patch.object(GitlabDataImporter, 'get_json_data')
+    def test_fetch_issues(self, mock_json):
+        mock_json.side_effect = [data.issues_json, []]
+
+        issues = self.api.fetch_issue([GitlabProject()])
+        assert mock_json.called
+        self.assertEqual(len(issues), 1)
+        self.assertEqual(issues[0].title, "title")
+        self.assertEqual(issues[0].description, "description")
+        self.assertEqual(issues[0].state, "opened")
+
+    @patch.object(GitlabDataImporter, 'get_json_data')
+    def test_fetch_comments_mr(self, mock_json):
+        mock_json.side_effect = [data.comment_mr_json, []]
+
+        comments_mr = self.api.fetch_comments_mr()
+        self.assertEqual(len(comments_mr), 1)
+        self.assertEqual(comments_mr[0].body, "message body")
+        self.assertEqual(comments_mr[0].user.username, "user")
+
+    @patch.object(GitlabDataImporter, 'get_json_data')
+    def test_fetch_comments_issues(self, mock_json):
+        mock_json.side_effect = [data.comment_issue_json, []]
+
+        comments_issue = self.api.fetch_comments_issues()
+        self.assertEqual(len(comments_issue), 1)
+        self.assertEqual(comments_issue[0].body, "message body")
+        self.assertEqual(comments_issue[0].user.username, "user")
+
+    def test_fill_object_data(self):
+        issue = GitlabIssue()
+
+        self.api.fill_object_data(data.issues_json[0], issue)
+        self.assertIsNotNone(issue.user)
+        self.assertEqual(issue.user.username, "user")
+        self.assertEqual(issue.created_at, parse("2014-10-11T16:25:37.548Z"))
+        self.assertEqual(issue.project_id, 32)
+        self.assertEqual(issue.title, "title")
