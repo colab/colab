caea32a61a46a7f7577ea2035c26ea5111ab335c
Sergio Oliveira <sergio@tracy.com.br>
2015-08-31 18:25:00 -0300

    Allow initconfig to execute on first run
    Signed-off-by: Sergio Oliveira <sergio@tracy.com.br>

--- colab/utils/runner.py
+++ colab/utils/runner.py
@@ -1,14 +1,20 @@
 
 import os
+import sys
 
 from django.core.management import ManagementUtility
-
+from colab.management.commands import initconfig
 
 def execute_from_command_line(argv=None):
     """
     A simple method that runs a ManagementUtility.
     """
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "colab.settings")
+    from django.conf import settings
 
-    utility = ManagementUtility(argv)
-    utility.execute()
+    if not hasattr(settings, 'SECRET_KEY') and 'initconfig' in sys.argv:
+        command = initconfig.Command()
+        command.handle()
+    else:
+        utility = ManagementUtility(argv)
+        utility.execute()
