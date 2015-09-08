a98dc763d17dbf2b852868c787b0ed13fe0c3607
Matheus Faria <matheus.sousa.faria@gmail.com>
2015-09-08 16:06:20 -0300

    Removing gitlab plugin folder

--- colab/plugins/gitlab/__init__.py
+++
@@ -1,8 +0,0 @@
-
-
-default_app_config = 'colab.plugins.gitlab.apps.ProxyGitlabAppConfig'
-
-from colab.plugins.utils.widget_manager import WidgetManager
-from colab.plugins.gitlab.widgets import GitlabProfileWidget
-
-WidgetManager.register_widget('profile', GitlabProfileWidget())
--- colab/plugins/gitlab/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.conf.urls import patterns, url
-
-from .views import GitlabProxyView, GitlabProfileProxyView
-
-urlpatterns = patterns('',
-    url(r'^(?P<path>.*)$', GitlabProxyView.as_view(), name='gitlab'),
-)
