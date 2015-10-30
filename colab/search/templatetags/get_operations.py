294aec50d612815a99b64f433705cba50444758f
Charles Oliveira <18oliveira.charles@gmail.com>
2015-10-30 09:57:32 -0400

    Removed everything related to superarchives

---
+++ colab/search/templatetags/get_operations.py
@@ -0,0 +1,46 @@
+
+from django import template
+import urllib
+import urlparse
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def append_to_get(context, **kwargs):
+    return append_to_get(
+        context['request'].META['PATH_INFO'],
+        context['request'].META['QUERY_STRING'],
+        **kwargs
+    )
+
+
+@register.simple_tag(takes_context=True)
+def pop_from_get(context, **kwargs):
+    return pop_from_get(
+        context['request'].META['PATH_INFO'],
+        context['request'].META['QUERY_STRING'],
+        **kwargs
+    )
+
+
+def append_to_get(path, query=None, **kwargs):
+    query_dict = dict(urlparse.parse_qsl(query))
+    for key, value in kwargs.items():
+        query_dict[key] = value
+    return u'{}?{}'.format(path, urllib.urlencode(query_dict))
+
+
+def pop_from_get(path, query=None, **kwargs):
+    query_dict = dict(urlparse.parse_qsl(query))
+    for key, value in kwargs.items():
+        if query_dict not in (key):
+            continue
+        if query_dict[key] == value:
+            del query_dict[key]
+            continue
+        if value in query_dict[key]:
+            aux = query_dict[key].split(value)
+            query_dict[key] = u''.join(aux).strip()
+    return u'{}?{}'.format(path, urllib.urlencode(query_dict))
