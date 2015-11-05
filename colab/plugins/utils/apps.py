3c3e0c07bc4838207b804f703ce7a19c77d5cbac
Lucas Moura <lucas.moura128@gmail.com>
2015-11-05 16:51:23 -0200

    Removed namespace from plugin settings
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- colab/plugins/utils/apps.py
+++ colab/plugins/utils/apps.py
@@ -1,9 +1,19 @@
 
 from django.apps import AppConfig
+from ..conf import get_plugin_config
 
 
 class ColabPluginAppConfig(AppConfig):
     colab_proxied_app = True
+    namespace = None
+
+    def __init__(self, app_name, app_module):
+        super(ColabPluginAppConfig, self).__init__(app_name, app_module)
+        self.set_namespace()
+
+    def set_namespace(self):
+        config = get_plugin_config(self.name)
+        config['urls']['namespace'] = self.namespace
 
     def register_signal(self):
         pass
