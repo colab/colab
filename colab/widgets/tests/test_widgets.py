da12803043d107891dcc932d2ca143422e39fd3f
Macartur Sousa <macartur.sc@gmail.com>
2015-11-16 16:53:44 -0200

    Adding tests for widget render content
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- colab/widgets/tests/test_widgets.py
+++ colab/widgets/tests/test_widgets.py
@@ -2,16 +2,22 @@ import unittest
 from mock import patch
 
 from colab.widgets.templatetags.widgets_tag import import_widgets
-from colab.widgets.widget_manager import WidgetManager
+from colab.widgets.widget_manager import WidgetManager, Widget
+from django.template import Context
+
+
+class WigetMock(Widget):
+    def __init__(self, content=""):
+        self.content = content
 
 
 class WidgetsTest(unittest.TestCase):
     @patch.object(WidgetManager, 'get_widgets')
     def test_import_widgets_tag(self, get_widgets):
-        return_list = [1, 2, 3]
+        return_list = [WigetMock(), WigetMock(), WigetMock()]
         get_widgets.return_value = return_list
 
-        context = {'request': ""}
+        context = Context({'request': ""})
         import_widgets(context, 'area')
 
         self.assertIn('widgets_area', context)
@@ -19,10 +25,10 @@ class WidgetsTest(unittest.TestCase):
 
     @patch.object(WidgetManager, 'get_widgets')
     def test_import_widgets_tag_with_named_var(self, get_widgets):
-        return_list = [1, 2, 3]
+        return_list = [WigetMock(), WigetMock(), WigetMock()]
         get_widgets.return_value = return_list
 
-        context = {'request': ""}
+        context = Context({'request': ""})
         import_widgets(context, 'area', 'var')
 
         self.assertIn('var', context)
