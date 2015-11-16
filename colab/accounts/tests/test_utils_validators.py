ee075ec1aa719cb5f2b840d0a5afd52ad20a0680
Lucas Kanashiro <kanashiro.duarte@gmail.com>
2015-11-16 16:10:13 -0200

    accounts: test validate_social_account method

---
+++ colab/accounts/tests/test_utils_validators.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+from ..utils.validators import validate_social_account
+
+
+class TestValidators(TestCase):
+
+    def test_validate_social_account_with_fake_account(self):
+       self.assertFalse(validate_social_account('john-fake',
+                                                'http://twitter.com'))
+
+    def test_validate_social_account(self):
+        self.assertTrue(validate_social_account('john', 'http://twitter.com'))
