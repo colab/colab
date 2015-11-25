d2f0c7bb9c5878be8e21935a2fe40f6aeab690b5
Sergio Oliveira <seocam@seocam.com>
2015-11-25 12:12:58 -0200

    New signal for user created

--- colab/accounts/signals.py
+++ colab/accounts/signals.py
@@ -1,5 +1,6 @@
 
-from django.dispatch import Signal
+from django.dispatch import receiver, Signal
+from django.db.models.signals import post_save
 
-
+user_created = Signal(providing_args=['user', 'password'])
 user_password_changed = Signal(providing_args=['user', 'password'])
