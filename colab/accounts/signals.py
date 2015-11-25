d0240e4b3c89b4492c8ca5001e2284c4031685ab
Sergio Oliveira <seocam@seocam.com>
2015-11-25 14:08:54 -0200

    Fixed flake8

--- colab/accounts/signals.py
+++ colab/accounts/signals.py
@@ -1,6 +1,6 @@
 
-from django.dispatch import receiver, Signal
-from django.db.models.signals import post_save
+from django.dispatch import Signal
+
 
 user_created = Signal(providing_args=['user', 'password'])
 user_password_changed = Signal(providing_args=['user', 'password'])
