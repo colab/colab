44542c1c193db337012c0f246ca14028d9452c4b
Lucas Moura <lucas.moura128@gmail.com>
2015-12-08 15:19:16 -0200

    Add template variable to Collaboration chart
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>
    Signed-off-by: Simião Carvalho <simiaosimis@gmail.com>

--- colab/accounts/widgets/collaboration_chart.py
+++ colab/accounts/widgets/collaboration_chart.py
@@ -1,9 +1,6 @@
 from colab.widgets.widget_manager import Widget
-from django.template.loader import render_to_string
 
 
 class CollaborationChart(Widget):
     name = "collaboration_chart"
-
-    def generate_content(self, **kwargs):
-        self.content = render_to_string('widgets/collaboration_chart.html')
+    template = 'widgets/collaboration_chart.html'
