49ce1a27bebdda8f17de26fb410946b698bc5b35
Sergio Oliveira <seocam@seocam.com>
2015-12-09 14:30:27 -0200

    Set STATIC_ROOT to /var/lib/colab/assets/static/
    Signed-off-by: Daniel Henrique <danielhmarinho@gmail.com>
    Signed-off-by: Lucas Kanashiro <kanashiro.duarte@gmail.com>
    Signed-off-by: Sergio Oliveira <seocam@seocam.com>

--- colab/settings.py
+++ colab/settings.py
@@ -81,7 +81,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = '/var/lib/colab/static/'
+STATIC_ROOT = '/var/lib/colab/assets/static/'
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = \
