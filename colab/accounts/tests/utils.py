a2e2e698dc4f96ac76e927ed6b4172884561dd5a
Lucas Moura <lucas.moura128@gmail.com>
2015-12-14 17:50:30 -0200

    Add test for username validators
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- colab/accounts/tests/utils.py
+++ colab/accounts/tests/utils.py
@@ -3,3 +3,7 @@ from django.forms import ValidationError
 
 def password_validator(password):
     raise ValidationError('Test error')
+
+
+def username_validator(username):
+    raise ValidationError('Test error')
