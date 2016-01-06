2a9ac4c6457f5b8448c633ad0ef9f2a54cefa4bb
Luiz Oliveira <ziuloliveira@gmail.com>
2016-01-06 15:42:29 -0200

    Fix widget
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>

--- colab/accounts/widgets/latest_contributions.py
+++ colab/accounts/widgets/latest_contributions.py
@@ -1,6 +1,16 @@
 from colab.widgets.widget_manager import Widget
+from colab.plugins.utils.collaborations import get_collaboration_data
 
 
 class LatestContributionsWidget(Widget):
     name = 'latest contributionsp'
     template = 'widgets/latest_contributions.html'
+
+    def generate_content(self, **kwargs):
+        collaborations, count_types_extras = get_collaboration_data(
+            kwargs['context']['user'], kwargs['context']['object'].username)
+
+        collaborations.sort(key=lambda elem: elem.modified, reverse=True)
+        kwargs['context']['results'] = collaborations[:10]
+
+        super(LatestContributionsWidget, self).generate_content(**kwargs)
