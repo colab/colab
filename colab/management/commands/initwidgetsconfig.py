b3ca91271385a323bf2815b8c5fdd8f6c661817a
Matheus Faria <matheus.sousa.faria@gmail.com>
2016-01-14 14:51:37 -0200

    Removing duplicated import from widgets config

--- colab/management/commands/initwidgetsconfig.py
+++ colab/management/commands/initwidgetsconfig.py
@@ -13,14 +13,12 @@ from colab.super_archives.widgets.dashboard_latest_threads import \
 from colab.super_archives.widgets.dashboard_collaboration_graph import \
     DashboardCollaborationGraphWidget
 
-from colab.widgets.widget_manager import WidgetManager
 from colab.accounts.widgets.group import GroupWidget
 from colab.accounts.widgets.group_membership import GroupMembershipWidget
 from colab.accounts.widgets.latest_posted import LatestPostedWidget
 from colab.accounts.widgets.latest_contributions import \
     LatestContributionsWidget
 
-from colab.widgets.widget_manager import WidgetManager
 from colab.accounts.widgets.collaboration_chart import CollaborationChart
 from colab.accounts.widgets.participation_chart import ParticipationChart
 
