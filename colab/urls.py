57e5d977763c826be6dc8ce3aaf970bb51d7e52c
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-02-19 15:48:41 -0200

    Added support to blacklist on core and plugins
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Simião Carvalho <simiaosimis@gmail.com>

--- colab/urls.py
+++ colab/urls.py
@@ -4,10 +4,32 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 from django.views.generic import RedirectView
 from accounts.views import UserProfileUpdateView
+from django.views.defaults import permission_denied
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = []
+
+colab_plugins = settings.COLAB_APPS
+
+for plugin in colab_plugins:
+    colab_plugin = colab_plugins.get(plugin)
+    plugin_blacklist = colab_plugin.get('blacklist')
+    if plugin_blacklist:
+        for plugin_url in plugin_blacklist:
+            final_url = colab_plugin.get('urls').get('prefix')
+            final_url += plugin_url
+            urlpatterns += patterns(
+                '', url(final_url, permission_denied))
+
+if hasattr(settings, 'BLACKLIST'):
+    core_blacklist = settings.BLACKLIST
+    for core_url in core_blacklist:
+        urlpatterns += patterns('', url(core_url, permission_denied))
+
+
+urlpatterns += patterns(
+    '',
     url(r'^$', RedirectView.as_view(url=settings.COLAB_HOME_URL), name='home'),
     url(r'^robots.txt$', 'colab.home.views.robots', name='robots'),
     url(r'^dashboard$', 'colab.home.views.dashboard', name='dashboard'),
