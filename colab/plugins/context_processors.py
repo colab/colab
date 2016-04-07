88d4fde3bd5d798577b94ac51b4d9921f046e214
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-04-07 15:32:42 -0300

    Added support to render different header
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- colab/plugins/context_processors.py
+++ colab/plugins/context_processors.py
@@ -1,5 +1,5 @@
-
 from django.conf import settings
+import re
 
 
 def colab_apps(request):
@@ -9,3 +9,21 @@ def colab_apps(request):
         colab_apps[app_name] = app
 
     return {'plugins': colab_apps}
+
+
+def get_prefixes():
+    prefixes = []
+    for plugin_name in settings.COLAB_APPS:
+        plugin = settings.COLAB_APPS[plugin_name]
+        prefix = plugin['urls']['prefix']
+        change_header = plugin.get('change_header', False)
+        if change_header:
+            prefixes.append(prefix)
+    return prefixes
+
+
+def change_header(request):
+    for prefix in get_prefixes():
+        if re.match(prefix, request.path[1:]):
+            return {'change_header': True}
+    return {'change_header': False}
