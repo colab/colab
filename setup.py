508f73a0d02f2601b7ba4175a670ad3e3b58636f
Macartur Sousa <macartur.sc@gmail.com>
2016-05-13 11:20:20 -0300

    Bump version 1.13.5 => 1.13.6
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ EXCLUDE_FROM_PACKAGES = []
 
 setup(
     name='colab',
-    version='1.13.5',
+    version='1.13.6',
     url='https://github.com/colab-community/colab',
     author='Sergio Oliveira',
     author_email='sergio@tracy.com.br',
